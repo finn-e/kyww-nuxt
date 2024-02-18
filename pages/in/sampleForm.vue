971cc35ba408cbbb572921fcafc058747bbcc024
Fin o'Flaherty <fin@oflaherty.is>
2024-02-18 14:26:25 -0500

    Stable, working tailwind+daisy

---
+++ pages/in/sampleForm.vue
@@ -0,0 +1,17 @@
+<template>
+<h1 class="fontSize-2xl">Kentucky Watershed Watch Monitoring Data Form</h1>
+<div class="grid grid-cols-4 border-10">
+			<form>
+				<div class="form-control">
+					<div class="white-space:nowrap">
+					<Icon name="uil:github" />
+					<label class="label">
+						<span class="before:content  label-text">Group Name</span>
+					</label>
+					<input type="select" class="select select-bordered" />
+				</div>
+				</div>
+			<button class="btn btn-primary">Submit</button>
+	</form>
+</div>
+</template>
---
+++ pages/test.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="wrapper">
+    <div class="wrapper__inner">
+      <Icon
+        class="wrapper__inner__icon"
+        name="logos:medium"
+        width="7em"
+        height="2.5em"
+        color="white"
+      />
+    </div>
+  </div>
+</template>
